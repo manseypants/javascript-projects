5f92a659b6aab68b26db8b119cdf6286ba0f6a78
manseypants <ridinspinaz04@gmail.com>
2023-11-16 17:52:01 -0600

    starting questions project

---
+++ 07-questions/app.js
@@ -0,0 +1,10 @@
+// using selectors inside the element
+// traversing the dom
+
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    console.log(e.currentTarget);
+  });
+});
